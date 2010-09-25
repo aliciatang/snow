50802a3880e56ef08e4997962608a2c6498b60d8
root <root@localhost.localdomain>
2010-09-25 16:11:10 -0700

    add transaction modle to backend

---
+++ apps/backend/modules/transaction/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/transactionGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/transactionGeneratorHelper.class.php';
+
+/**
+ * transaction actions.
+ *
+ * @package    valueInvest
+ * @subpackage transaction
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class transactionActions extends autoTransactionActions
+{
+}
---
+++ apps/backend/modules/transaction/lib/transactionGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * transaction module helper.
+ *
+ * @package    valueInvest
+ * @subpackage transaction
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class transactionGeneratorHelper extends BaseTransactionGeneratorHelper
+{
+}
